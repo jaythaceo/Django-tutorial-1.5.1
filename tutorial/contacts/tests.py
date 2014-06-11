ee03aa9b38d2332cbd30dc10e2d8bb15244d8f5b
jaythaceo <jaythaceo@gmail.com>
2014-06-11 14:28:38 -0400

    Major additions

---
+++ tutorial/contacts/tests.py
@@ -0,0 +1,19 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+from django.test import TestCase
+
+from contacts.models import Contact
+
+class ContactsTests(TestCase):
+
+    def test_str(self):
+        contact = Contact(first_name='John', last_name='Smith')
+
+        self.assertEquals(
+            str(contact),
+            'John Smith',
+        )
